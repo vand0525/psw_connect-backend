4a4c195a84950486ac9e39f2305103ab1b9446aa
Vand0525 <vand0525@algonquinlive.com>
2025-03-15 19:42:18 -0400

    I think we cooked

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ const jobRouter = require('./routers/job');
 const userRouter = require('./routers/user');
 const { errorHandler } = require('./middleware/errors');
 const { connect } = require('./models/db');
-const { auth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 const { syncUser } = require('./middleware/sync');
 const cors = require('cors');
-
 
 console.log(process.env.MONGO_URL);
 
@@ -23,15 +22,8 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use(
-	cors({
-		origin: 'http://localhost:5500',
-		credentials: true,
-	})
-);
-
-app.use(
 	auth({
-		authRequired: true,
+		authRequired: false,
 		auth0Logout: true,
 		secret: process.env.AUTH0_SECRET,
 		baseURL: process.env.BASE_URL,
@@ -40,17 +32,29 @@ app.use(
 	})
 );
 
+const corsOptions = {
+    origin: 'http://localhost:5500/', // Replace with your frontend URL
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.get('/logout', (req, res) => {
+    req.oidc.logout({ returnTo: process.env.BASE_URL});
+    res.send('Logged out');
+});
+
 app.use(syncUser);
 
 app.get('/api/me', (req, res) => {
 	res.json({
-		user: req.oidc.user,
-		idToken: req.oidc.idToken,
+		user: req.oidc.user || null,
+		idToken: req.oidc.idToken || null,
 		accessToken: req.oidc.accessToken ? req.oidc.accessToken.access_token : null,
 	});
 });
 
-app.use('/api/jobs', jobRouter);
+app.use('/api/jobs', requiresAuth(), jobRouter);
 // app.use('/api/users', userRouter);
 
 app.use(errorHandler);
